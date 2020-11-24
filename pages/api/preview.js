50014f9555ea5eef69aed38629bc7865b0ecd12e
Lee Campbell <leecampbellr@gmail.com>
2020-11-23 22:55:10 -0500

    preview second page

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,14 +1,27 @@
+import { gql } from 'graphql-request';
+import cms from '@utils/cms';
 
 export default async function handler(req, res) {
 
   if (!req.query.entryUid) {
 		return res.status(401).json({ message: "Not allowed to access this route" })
   }
-  
+
   const { entryUid, token } = req.query;
 
+	const query = gql`
+		query($uid: [String]) {
+			entry(uid: $uid) {
+				id
+				uri
+			}
+		}
+  `
+  
+  const data = await client.request(pageQuery, { uid: entryUid })
+
   res.setPreviewData({ entryUid, token });
-  res.redirect('/');
+  res.writeHead(307, { Location: `${data.entry.uri}` })
   res.end();
 
 }
