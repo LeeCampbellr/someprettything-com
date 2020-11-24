99b2fc3d7cfb89833a629092be1695181b042975
Lee Campbell <leecampbellr@gmail.com>
2020-11-24 14:53:07 -0500

    stupid extra space

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const { entryUid, token } = req.query;
 
   const query = gql`
-    query (uid: [String]) {
+    query(uid: [String]) {
         entry(uid: $uid)  {
           id
           slug
