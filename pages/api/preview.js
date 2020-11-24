a51b2f07ea714b8f6c5f2d599664ace46ac916a1
Lee Campbell <leecampbellr@gmail.com>
2020-11-23 20:35:43 -0500

    clearing out the preview token

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,7 +1,7 @@
 
 export default async function handler(req, res) {
 
-  if (!req.query.entryUid || !req.query.token) {
+  if (!req.query.entryUid) {
 		return res.status(401).json({ message: "Not allowed to access this route" })
   }
   
