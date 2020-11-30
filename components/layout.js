720f0058baa782b40cb4b1515aa113b0e33fd177
Lee Campbell <leecampbellr@gmail.com>
2020-11-29 22:15:44 -0500

    craft preview - scroll position

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from 'next/head';
 
 import Navigation from "@organisms/navigation"
 import Footer from "@organisms/footer"
@@ -6,6 +7,9 @@ import Footer from "@organisms/footer"
 function Layout({ children }) {
   return (
     <React.Fragment>
+      <Head>
+        <script type="text/javascript" src="/static/iframeresizer.js"></script>
+      </Head>
       <Navigation/>
       <main>{children}</main>
       <Footer/>
